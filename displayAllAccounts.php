afd2572375f263f3cca2c2ee3976419b63e103a4
ryangill <gillryan342@outlook.com>
2024-05-16 14:26:10 +0100

    Deleted deletedriver as admin can now delete account from displayaccounts php

--- displayAllAccounts.php
+++ displayAllAccounts.php
@@ -11,7 +11,93 @@ function getAccounts($mysqli) {
     return $mysqli->query($query);
 }
 
-$accounts = getAccounts($mysqli);
+try {
+    if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["UserID"])) {
+        $userID = $mysqli->real_escape_string($_POST["UserID"]);
+
+        $stme = $mysqli->prepare("SELECT Email FROM account WHERE UserID=?");
+        $stme->bind_param("i", $userID);
+        $stme->execute();
+        $emailResult = $stme->get_result();
+        $emailRow = $emailResult->fetch_assoc();
+        $email = $emailRow["Email"];
+        $mail = require __DIR__ . "/mailer.php";
+        $mail->setFrom("parklyuser@outlook.com");
+        $mail->addAddress($email);
+        $mail->Subject = "Account Deleted";
+        $mail->Body = <<<EOD
+<html>
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+    <title>Account Deletion</title>
+    <meta name="description" content="Booking Successful">
+    <style type="text/css">
+        a:hover {text-decoration: underline !important;}
+    </style>
+</head>
+<body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+    <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8" style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+        <tr>
+            <td>
+                <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0" align="center" cellpadding="0" cellspacing="0">
+                    <tr><td style="height:80px;">&nbsp;</td></tr>
+                    <tr>
+                        <td style="text-align:center;">
+                          <img width="60" src="logoLeft.png" alt="Logo">
+                        </td>
+                    </tr>
+                    <tr><td style="height:20px;">&nbsp;</td></tr>
+                    <tr>
+                        <td>
+                            <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0" style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                <tr><td style="height:40px;">&nbsp;</td></tr>
+                                <tr>
+                                    <td style="padding:0 35px;">
+                                        <h1>Account Deletion</h1>
+                                        <p>Your Account was Deleted</p>
+                                            We're sorry to see you go. You have been removed from our system.</p>
+                                            If you would like to contact us about this matter please click the link below:
+                                        </p>
+                                        <a href="https://localhost/contactDriver.php" style="background:#20e277;text-decoration:none; color:#fff; padding:10px 24px; border-radius:50px; display:inline-block;">Contact Us</a>
+                                    </td>
+                                </tr>
+                                <tr><td style="height:40px;">&nbsp;</td></tr>
+                            </table>
+                        </td>
+                    </tr>
+                    <tr><td style="height:20px;">&nbsp;</td></tr>
+                    <tr><td style="height:80px;">&nbsp;</td></tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
+EOD;
+        try {
+            $mail->send();
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer error: {$mail->ErrorInfo}";
+            exit;
+        }
+
+
+        $deleteQuery = "DELETE FROM account WHERE UserID = '$userID'";
+
+        $deleteResult = $mysqli->query($deleteQuery);
+
+        if ($deleteResult === false) {
+            throw new mysqli_sql_exception("Failed to Delete Account: " . $mysqli->error);
+        }
+
+
+    }
+
+    $accounts = getAccounts($mysqli);
+
+} catch (mysqli_sql_exception $e) {
+    $errorMessage = "Error: Unable to delete Account. It might be associated with other records. " . $e->getMessage();
+}
 ?>
 
 <body>
@@ -35,6 +121,7 @@ $accounts = getAccounts($mysqli);
                 <th>Username</th>
                 <th>Email</th>
                 <th>PhoneNumber</th>
+                <th>Actions</th>
             </tr>
             <?php while ($row = $accounts->fetch_assoc()): ?>
                 <tr>
@@ -46,22 +133,18 @@ $accounts = getAccounts($mysqli);
                     <td><?= htmlspecialchars($row['Username']) ?></td>
                     <td><?= htmlspecialchars($row['Email']) ?></td>
                     <td><?= htmlspecialchars($row['PhoneNumber']) ?></td>
-<!--                    <td>-->
-<!--                        <form method="post">-->
-                            <!-- Use LicensePlate to identify the car to be removed -->
-<!--                            <input type="hidden" name="UserID" value="--><?php //= htmlspecialchars($row['LicensePlate']) ?><!--">-->
-<!--                            <button type="submit">Remove</button>-->
-<!--                        </form>-->
-<!--                    </td>-->
+                    <td>
+                        <form method="post">
+                            <input type="hidden" name="UserID" value="<?= htmlspecialchars($row['UserID']) ?>">
+                            <button type="submit">Remove</button>
+                        </form>
+                    </td>
                 </tr>
             <?php endwhile; ?>
         </table>
     <?php else: ?>
         <p class="text">No Accounts Found.</p>
     <?php endif; ?>
-    <a id="acc_btn" class = "button" href="deleteDriver.php"> Delete an Account</a>
-
-
 </div>
 
 
